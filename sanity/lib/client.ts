206cc40207ee70f3a23941d9ba3c360ea3eb18c3
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2023-09-22 16:17:30 +0200

    fix token error

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token: "skQTqB6hvaiVaHEUftQjBc9y6RrUsVmeLPBN3scR13CSnobks4A9IIpdruy2FfgA1IY59dp2diA41u9GJdCRsjxKKGrox8EstsYuxHtMvHwtKe1krzDnjmYUiem31oXRPL4kQvdF6epuYc5YThOZsAsAVzceYhycbhGnCyI2579910DfJSvi",
+  token: process.env.SANITY_TOKEN,
 });
