4e422b43d93cbc5e422e3c0816e1770722810ae5
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2023-09-25 15:22:30 +0200

    add feature product sort

--- src/components/ProductSort.component.tsx
+++ src/components/ProductSort.component.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+"use client";
 
 const sortOptions = [
   { name: "Newest", value: "/?date=desc" },
@@ -9,10 +9,18 @@ const sortOptions = [
 export const ProductSort = () => {
   return (
     <div className="flex items-center">
-      <select className="form-select w-auto" aria-label="Default select">
-        <option>Sort By</option>
+      <select
+        className="form-select w-auto"
+        onChange={(e) => {
+          const selectedValue = e.target.value;
+          window.location.href = selectedValue;
+        }}
+        aria-label="Default select">
+        <option value="" hidden>
+          Sort By
+        </option>
         {sortOptions.map((option) => (
-          <option key={option.name} value={option.value}>
+          <option key={option.value} value={option.value}>
             {option.name}
           </option>
         ))}
