fa06c177f6134dbe5cf22db3f51632fc94ec0f94
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2023-10-06 16:13:48 +0200

    add Stripe checkout

--- src/components/CartSummary.component.tsx
+++ src/components/CartSummary.component.tsx
@@ -5,16 +5,49 @@ import { Button, Spinner } from "react-bootstrap";
 import { formatCurrencyString, useShoppingCart } from "use-shopping-cart";
 
 export const CartSummary = () => {
-  const { formattedTotalPrice, totalPrice, cartDetails, cartCount } = useShoppingCart();
-  const [isLoading, setIsLoading] = useState(false);
+  const { formattedTotalPrice, totalPrice, cartDetails, cartCount, redirectToCheckout } = useShoppingCart();
+  const [isLoading, setLoading] = useState(false);
   const isDisabled = isLoading || cartCount! === 0;
   const shippingAmount = cartCount! > 0 ? 500 : 0;
   const totalAmoumt = totalPrice! + shippingAmount;
 
-  const onCheckout = () => {};
+  async function onCheckout() {
+    setLoading(true);
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      body: JSON.stringify(cartDetails),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const result = await redirectToCheckout(data.id);
+    if (result?.error) {
+      console.error(result);
+    }
+    setLoading(false);
+  }
+
+  // async function onCheckout() {
+  //   setLoading(true);
+  //   const response = await fetch("/api/checkout", {
+  //     method: "POST",
+  //     body: JSON.stringify(cartDetails),
+  //   });
+  //   const responseData = await response.text();
+  //   if (responseData) {
+  //     const data = JSON.parse(responseData);
+  //   } else {
+  //     console.error("Empty response from server");
+  //   }
+  //   setLoading(false);
+  // }
 
   return (
     <>
+      <form action="/api/checkout/route" method="POST"></form>
       <div className="border rounded-3 p-4">
         <h5>Order summary</h5>
         <dl>
@@ -30,12 +63,12 @@ export const CartSummary = () => {
           <div className="d-flex align-items-center justify-content-between border-bottom p-3">
             <dt>Order total</dt>
             <dd className="m-0">{formatCurrencyString({ value: totalAmoumt, currency: "PLN" })}</dd>
-
           </div>
         </dl>
 
         <Button onClick={onCheckout} style={{ width: "100%" }} disabled={isDisabled}>
           {isLoading && <Spinner as="span" animation="grow" size="sm" role="status" aria-hidden="true" />}
+
           {isLoading ? "Loading..." : "Checkout"}
         </Button>
       </div>
