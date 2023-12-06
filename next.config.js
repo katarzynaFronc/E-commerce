6c7eb5ea1ef97c6ea16cee489c8b974960e2b382
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2023-12-06 14:53:18 +0100

    fix CORS

--- next.config.js
+++ next.config.js
@@ -18,3 +18,11 @@ var cors = require("cors");
 var app = express();
 
 app.use(cors());
+
+app.get("/products/:id", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
+app.listen(80, function () {
+  console.log("CORS-enabled web server listening on port 80");
+});
