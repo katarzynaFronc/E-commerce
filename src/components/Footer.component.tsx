ee301df77eeafc700f63b26a5c661ae77328c4c9
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2024-01-19 15:14:21 +0100

    add footer

--- src/components/Footer.component.tsx
+++ src/components/Footer.component.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+
 export const Footer = () => {
-  return <div className="footer-container">footer</div>;
+  return (
+    <div className="footer-container container-fluid  align-items-end">
+      <div className="row justify-content-around px-5 py-3">
+        <div className="col-md-8">
+          <span className="small">Copyright © 2024 KF</span>
+          <p className="small">All rights reserved</p>
+        </div>
+        <div className="col-md-2">
+          <span>Contact me:</span>
+          <div>
+            <a href="https://pl.linkedin.com/in/katarzyna-f">
+              <picture>
+                <img src="/icon/iconmonstr-linkedin-4-240.png" alt="LinkedIn" className="footer-icon"></img>
+              </picture>
+            </a>
+            <a href="https://github.com/katarzynaFronc">
+              <picture>
+                <img src="/icon\github-mark.png" alt="GitHub" className="footer-icon"></img>
+              </picture>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
